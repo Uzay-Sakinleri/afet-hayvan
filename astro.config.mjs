ff6ab9b1a4f777b64987671d8b63766c8890eb07
Ertan Taner <62712555+Ertan222@users.noreply.github.com>
2023-04-01 20:26:32 +0300

    feat: add grid layout basic (#5)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import netlify from "@astrojs/netlify/functions";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), image()],
   output: "server",
   adapter: netlify()
 });
