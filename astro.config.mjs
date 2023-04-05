5430c1dc50d616e457cd6c636bbaf536889d0178
Ertan Taner <ertantaner2000@gmail.com>
2023-04-05 19:55:31 +0300

    chore: remove space

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
-
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
