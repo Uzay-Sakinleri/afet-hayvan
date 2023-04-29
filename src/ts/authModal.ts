c14496609b94aeeaf2c2226cb512cf67da8b27df
Ertan Taner <ertantaner2000@gmail.com>
2023-04-29 20:43:26 +0300

    feat: add validation and error displaying

--- src/ts/authModal.ts
+++ src/ts/authModal.ts
@@ -2,38 +2,46 @@ const closeModalRegister = document.querySelector<HTMLSpanElement>("#close_modal
 const closeModalLogin = document.querySelector<HTMLSpanElement>("#close_modal_login");
 const registerFormBtn = document.querySelector<HTMLButtonElement>("#register_form_btn");
 const loginFormBtn = document.querySelector<HTMLButtonElement>("#login_form_btn");
-const registerDiv = document.querySelector<HTMLDivElement>("#register_div");
-const loginDiv = document.querySelector<HTMLDivElement>("#login_div");
 const loginBtn = document.querySelector<HTMLButtonElement>("#login_btn");
 
 export const providerArea = document.querySelector<HTMLDivElement>("#provider_area");
 export const loginForm = document.querySelector<HTMLFormElement>("#login_form");
 export const registerForm = document.querySelector<HTMLFormElement>("#register_form");
 export const authModal = document.querySelector<HTMLDivElement>("#auth_modal");
+export const registerDiv = document.querySelector<HTMLDivElement>("#register_div");
+export const loginDiv = document.querySelector<HTMLDivElement>("#login_div");
 
 loginBtn.addEventListener("click", () => {
     authModal.classList.replace("hidden", "grid");
-})
+});
 
 closeModalRegister.addEventListener("click", () => {
     authModal.classList.replace("grid", "hidden");
+    registerForm.querySelectorAll("input").forEach(e => e.value = "");
+    registerForm.querySelector(".result")?.remove();
 });
 
-closeModalLogin.addEventListener("click", () => {
+closeModalLogin.addEventListener("click", () =>  {
     authModal.classList.replace("grid", "hidden");
+    loginForm.querySelectorAll("input").forEach(e => e.value = "");
+    loginForm.querySelector(".result")?.remove();
 });
 
 document.addEventListener("click", (e: MouseEvent) => {
     if (e.target === authModal)
-        authModal.classList.add("hidden");
+        authModal.classList.replace("grid", "hidden");
 });
 
 registerFormBtn.addEventListener("click", () => {
     loginDiv.classList.replace("flex", "hidden");
+    loginForm.querySelectorAll("input").forEach(e => e.value = "");
+    loginForm.querySelector(".result")?.remove();
     registerDiv.classList.replace("hidden", "flex");
 });
 
 loginFormBtn.addEventListener("click", () => {
     registerDiv.classList.replace("flex", "hidden");
+    registerForm.querySelectorAll("input").forEach(e => e.value = "");
+    registerForm.querySelector(".result")?.remove();
     loginDiv.classList.replace("hidden", "flex");
 });
