19bb68306851bda2aa8caad7c9033d268578b5f8
Ertan Taner <ertantaner2000@gmail.com>
2023-04-30 01:49:48 +0300

    build: add conditional building

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,16 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import vercel from "@astrojs/vercel/serverless";
 import node from "@astrojs/node";
+import { loadEnv } from "vite";
+const env = loadEnv(import.meta.env.MODE, process.cwd(), "" );
+const isNode = env.IS_NODE === "true";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), image()],
   output: "server",
-  adapter: process.env.NODE ? node({mode: "middleware"}) : vercel(),
+  adapter: isNode ? node({mode: "middleware"}) : vercel(),
   image: {
-    service: process.env.NODE ? "astro/assets/services/sharp": "astro/assets/services/squoosh"
+    service: isNode ? "astro/assets/services/sharp": "astro/assets/services/squoosh"
   }
 });
