ecbe32096bb308f8c264c197f7604aadb718f0fb
Ertan Taner <ertantaner2000@gmail.com>
2023-05-01 03:07:08 +0300

    fix: delete unused props from firebase config

--- services/firebase/firebase.ts
+++ services/firebase/firebase.ts
@@ -3,12 +3,10 @@ const env = import.meta.env;
 const firebaseConfig = {
   apiKey: env.PUBLIC_API_KEY,
   authDomain: env.PUBLIC_AUTH_DOMAIN,
-  databaseURL: env.PUBLIC_DATABASE_URL,
   projectId: env.PUBLIC_PROJECT_ID,
   storageBucket: env.PUBLIC_STORAGE_BUCKET,
   messagingSenderId: env.PUBLIC_MESSAGING_SENDER_ID,
   appId: env.PUBLIC_APP_ID,
-  measurementId: env.PUBLIC_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
